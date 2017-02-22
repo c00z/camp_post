9594934c0b08d10b7b7765cd887c0e6331780e30
c00z <zcusimano@gmail.com>
2017-02-21 18:40:34 -0800

    logout and log-in working

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'welcome#index'
+
+
+  devise_scope :user do
+  get 'login', to: 'devise/sessions#new'
+  get 'signup', to: 'devise/registrations#new'
+  get 'logout', to: 'devise/sessions#destroy'
+  end
+
+  devise_for :users, path_names: {password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
 end
