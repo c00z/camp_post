506c33e6638eeb00cdbb6774d3faf058afcd58cc
c00z <zcusimano@gmail.com>
2017-02-22 10:19:22 -0800

    setup all routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,19 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   get '/users/:id' => 'users#show', as: 'user'
+  get '/users/:id/edit' => 'users#edit', as: 'edit_user'
+  patch '/users/:id' => 'users#update'
 
+  get '/campsites/:id' => 'locations#show', as: 'location'
+  get '/campsites/:id/reviews/new' => 'reviews#new', as: 'new_review'
+  post '/campsites/:id/reviews' => 'reviews#create', as: 'create_review'
+
+  get '/reviews' => 'reviews#index', as: 'reviews'
+  get '/reviews/:id' => 'reviews#show', as: 'review'
+  get '/reviews/:id/edit' => 'reviews#edit', as: 'edit_review'
+  patch '/reviews/:id' => 'reviews#update'
+  delete '/reviews/:id' => 'reviews#destroy'
+  
   devise_scope :user do
   get 'login', to: 'devise/sessions#new'
   get 'signup', to: 'devise/registrations#new'
