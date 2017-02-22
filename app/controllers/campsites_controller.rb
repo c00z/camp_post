61169c0156f0bd5b28ef77c16efdada9a7ca8664
c00z <zcusimano@gmail.com>
2017-02-22 13:51:21 -0800

    added foreign keys to posts

--- app/controllers/campsites_controller.rb
+++ app/controllers/campsites_controller.rb
@@ -1,9 +1,17 @@
 class CampsitesController < ApplicationController
 
+  require 'rubygems'
+  require 'nokogiri'
+  require 'open-uri'
+
   def index
-    # @locations = Location.all
-    # render :index
+  wiki_url = "https://en.wikipedia.org/wiki/List_of_national_parks_of_the_United_States"
+  page = Nokogiri::HTML(open(wiki_url))
+    @campsites = page.css("tr").each do |el|
+     puts el.text
+    end
   end
+
 
   def show
     @campsites = Campsite.find_by_id(params[:id])
