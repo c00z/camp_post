a9409ce90ac399da7e7a0fcb6d532da5b03d486a
c00z <zcusimano@gmail.com>
2017-02-22 16:13:30 -0800

    post showing up on campsite page

--- app/controllers/campsites_controller.rb
+++ app/controllers/campsites_controller.rb
@@ -7,7 +7,12 @@ class CampsitesController < ApplicationController
   def index
   wiki_url = "https://en.wikipedia.org/wiki/List_of_national_parks_of_the_United_States"
   page = Nokogiri::HTML(open(wiki_url))
-  @campsites = page.css("tr th a").text
+  campsites = page.css("tr th a")
+  @campsite_array = []
+  campsites.map do |a|
+    campsite_name = a.text
+    @campsite_array.push(campsite_name)
+  end
   end
 
 
