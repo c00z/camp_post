862e6d9deb8703c0435deac56117af65248148e3
c00z <zcusimano@gmail.com>
2017-02-23 14:29:02 -0800

    switching branches for bookmark

---
+++ app/controllers/campsites_users_controller.rb
@@ -0,0 +1,12 @@
+class CampsitesUsersController < ApplicationController
+
+  # before_action :require_login, only: [:build]
+  #
+  # def build
+  #   @campsite = Campsite.find(params[:id])
+  #   @user = current_user
+  #   @campsite.users << @user
+  #   redirect_to :back
+  # end
+
+end
