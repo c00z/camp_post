e822f7b4d61de26290de3399c565d801f0e7bcea
c00z <zcusimano@gmail.com>
2017-02-24 11:21:44 -0800

    working aside from campsite name

--- app/controllers/campsites_users_controller.rb
+++ app/controllers/campsites_users_controller.rb
@@ -4,9 +4,6 @@ class CampsitesUsersController < ApplicationController
 
   def build
     @campsite = Campsite.find_by_id((params[:campsite_id]))
-    p params
-    p "this is that thing"
-    p @campsite
     @user = current_user
     @campsite.users << @user
     redirect_to :back
