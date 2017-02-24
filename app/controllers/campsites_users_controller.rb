85b035e285c8d7ab001bb7e298a9e8684fea800e
c00z <zcusimano@gmail.com>
2017-02-24 11:10:19 -0800

    bookmarking working

--- app/controllers/campsites_users_controller.rb
+++ app/controllers/campsites_users_controller.rb
@@ -3,10 +3,12 @@ class CampsitesUsersController < ApplicationController
   # before_action :require_login, only: [:build]
 
   def build
-    @campsite = Campsite.find_by_id((params[:id]))
+    @campsite = Campsite.find_by_id((params[:campsite_id]))
+    p params
+    p "this is that thing"
     p @campsite
     @user = current_user
-    @campsites.users << @user
+    @campsite.users << @user
     redirect_to :back
   end
 
