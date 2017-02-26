ce6a40f73157c50e410c3f65377470b209b9fe4b
c00z <zcusimano@gmail.com>
2017-02-26 09:55:40 -0800

    controllers updated

--- app/controllers/camping_lists_controller.rb
+++ app/controllers/camping_lists_controller.rb
@@ -6,7 +6,15 @@ class CampingListsController < ApplicationController
   end
 
   def create
+    new_list = List.new(list_params)
+    @user = User.find_by_id(params[:id])
     @camping_lists = CampingList.create(list_params)
+    new_list.user = current_user
+  end
+
+  def new
+    @camping_lists = List.new
+    @user = User.find_by_id(params[:id])
   end
 
 
