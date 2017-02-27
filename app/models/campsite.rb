44e0c331a94800ea1ec14a10fdb32a97bf35deee
c00z <zcusimano@gmail.com>
2017-02-27 12:55:10 -0800

    search bar working

--- app/models/campsite.rb
+++ app/models/campsite.rb
@@ -3,4 +3,8 @@ class Campsite < ApplicationRecord
   has_many :campsites_users
   has_many :users, through: :campsites_users
 
+  def self.search(search)
+    where("name ILIKE ?", "%#{search}%")
+  end
+
 end
